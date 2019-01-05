82cea92d7f3a6674865a4526c132aca1105d78bd
Alex Ciminian <alex.ciminian@gmail.com>
2019-01-05 07:00:08 +0000

    🆕 Update GitHub pages site

--- frag/randoms/maze-fill/display.frag
+++
@@ -1,7 +0,0 @@
-precision mediump float;
-uniform sampler2D state;
-void main(void) {
-  vec2 coord = vec2(gl_FragCoord)/64.0;
-  gl_FragColor = texture2D(state, coord);
-  // gl_FragColor = vec4(vec3(0.0, 0.0, 1.0), 1.0);
-}
---
+++ precache-manifest.2031d32cc2b1d4b35052b373a5f618cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8900114236386198793",
+    "url": "/shaders/static/js/runtime~main.e8900114.js"
+  },
+  {
+    "revision": "c4d0d703e098e54423b9",
+    "url": "/shaders/static/js/main.c4d0d703.chunk.js"
+  },
+  {
+    "revision": "40f999b7f4a52e55d5ec",
+    "url": "/shaders/static/js/1.40f999b7.chunk.js"
+  },
+  {
+    "revision": "c4d0d703e098e54423b9",
+    "url": "/shaders/static/css/main.6770d19f.chunk.css"
+  },
+  {
+    "revision": "58a2179dfd1438122b7d603d1877322f",
+    "url": "/shaders/index.html"
+  }
+];

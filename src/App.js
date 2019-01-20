590d627387fdb8b63715b1c0ac6b89bc4bc7283e
Alex Ciminian <alex.ciminian@gmail.com>
2019-01-20 16:54:24 +0000

    Switch to using HashRouter for GHPages compat

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 import { GalleryItem } from "./components/GalleryItem";
 import { GlslModal } from "./components/GlslModal";
 import { GameOfLife } from "./components/gallery/GameOfLife";
